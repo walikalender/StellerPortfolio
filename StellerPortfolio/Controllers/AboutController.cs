e9a09a1fdb04c0ab643214f43c0b5abe5a9d1431
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-03-11 21:53:14 +0300

    Add project files.

---
+++ StellerPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using StellerPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StellerPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        StellerAcunMedyaDBEntities db = new StellerAcunMedyaDBEntities();
+        public ActionResult Index()
+        {
+
+            var aboutList = db.TblAbouts.ToList();
+            return View(aboutList);
+        }
+        public ActionResult DeleteAbout(int id)
+        {
+            var deletedEntity = db.TblAbouts.Find(id);
+            db.TblAbouts.Remove(deletedEntity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StellerPortfolio/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StellerPortfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
