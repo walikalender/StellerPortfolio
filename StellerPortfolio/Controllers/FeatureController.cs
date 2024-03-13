94544e1aebc6c9eacd08e1830ce57a35f172eff9
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-03-13 15:13:27 +0300

    + Completed Feature CRUD operation

--- StellerPortfolio/Controllers/FeatureController.cs
+++ StellerPortfolio/Controllers/FeatureController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StellerPortfolio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,52 @@ namespace StellerPortfolio.Controllers
 {
     public class FeatureController : Controller
     {
+        StellerAcunMedyaDBEntities db = new StellerAcunMedyaDBEntities();
         public ActionResult Index()
         {
+            var featureList = db.TblFeatures.ToList();
+
+            return View(featureList);
+        }
+        public ActionResult DeleteFeature(int id)
+        {
+            var deleteEntity = db.TblFeatures.Find(id);
+            db.TblFeatures.Remove(deleteEntity);
+            db.SaveChanges();
+            return RedirectToAction("Index");  
+        }
+        [HttpGet]
+        public ActionResult AddFeature()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddFeature(TblFeatures tblFeatures)
+        {
+            db.TblFeatures.Add(tblFeatures);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateFeature(int id)
+        {
+           var entity = db.TblFeatures.Find(id);
+            return View(entity);
+        }
+        [HttpPost]
+        public ActionResult UpdateFeature(TblFeatures tblFeatures)
+        {
+            var updatedEntity = db.TblFeatures.Find(tblFeatures.FeatureID);
+
+            updatedEntity.FirstName=tblFeatures.FirstName;
+            updatedEntity.LastName=tblFeatures.LastName;
+            updatedEntity.Job=tblFeatures.Job;
+            updatedEntity.Title=tblFeatures.Title;
+            updatedEntity.CvDownloadUrl=tblFeatures.CvDownloadUrl;
+            updatedEntity.ImageUrl=tblFeatures.ImageUrl;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");   
         }
     }
 }
